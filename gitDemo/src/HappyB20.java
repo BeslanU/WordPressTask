9a2e5e88e508724b6203e0e9898d2a35c4a015f5
Wildhunt3 <92beslan@gmail.com>
2020-08-01 10:59:41 -0500

    update HappyB20

--- gitDemo/src/HappyB20.java
+++ gitDemo/src/HappyB20.java
@@ -1,5 +1,7 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("Helo");
+        System.out.println("Java");
     }
 }
