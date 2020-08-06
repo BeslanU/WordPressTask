8ae5d25337378d9c0efb5f217e97795f7ef8aee7
Wildhunt3 <92beslan@gmail.com>
2020-08-05 20:25:06 -0500

    add MissJava.java

---
+++ Summer2020_B20_git/src/Utility/MissJava.java
@@ -0,0 +1,9 @@
+package Utility;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class MissJava {
+    public static void main(String[] args) {
+        System.out.println("I miss java so bad");
+    }
+}
