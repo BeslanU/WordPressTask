0c687d094ad264c0bc1b04c195c367fd6f0b7c73
BeslanU <92beslan@gmail.com>
2021-04-23 01:54:49 -0500

    Hello

--- Summer2020_B20/src/day0/MethodCall.java
+++ Summer2020_B20/src/day0/MethodCall.java
@@ -1,13 +1,50 @@
 package day0;
 
+import java.util.Scanner;
+
 public class MethodCall {
 
     public static void main(String[] args) {
-
-        String str = "Cybertek School";
-
-
-        String str2 = "abcabcaaaaabbbbbbcccccddddeeeeeee";
+        Scanner scan = new Scanner(System.in);
+        String  input = scan.nextLine();
+        int giftCard = 100;
+        if(input.equals("Smartphone")|| input.equals("Loptop")){
+            System.out.println("Sorry, not enough funds on your gift card!");
+        }else if(input.equals("Charger")){
+            giftCard-=15.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 85$");
+        }else if(input.equals("USB cable")){
+            giftCard-=10.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 90$");
+        }else if(input.equals("Headphones")){
+            giftCard-=30.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 70$");
+        }else if(input.equals("Pants")){
+            giftCard-=50.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 50$");
+        }else if(input.equals("Hat")){
+            giftCard-=25.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 75$");
+        }else if(input.equals("Socks")){
+            giftCard-=5.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 95$");
+        }else if(input.equals("Blanket")){
+            giftCard-=60.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 40$");
+        }else if(input.equals("Pillow")){
+            giftCard-=40.00;
+            System.out.println("Thank you for your purchase!");
+            System.out.println("Your current balance is: 60$");
+        }else {
+            System.out.println("Invalid item!");
+        }
 
 
     }
--- Summer2020_B20/src/day0/Test.java
+++ Summer2020_B20/src/day0/Test.java
@@ -1,7 +1,31 @@
 package day0;
 
+import java.util.Scanner;
+
 public class Test {
 
+    public static void main(String[] args) {
+         int CANDY_PRICE = 10;
+         int GUM_PRICE = 3;
+        //DO NOT CHANGE
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter number of coupons:");
+        int coupons = scan.nextInt();
+         if (coupons>2){
+    int candyAV = coupons/CANDY_PRICE;
+    int gumAV = (coupons%CANDY_PRICE)/GUM_PRICE;
+    int couponLeft = (coupons%CANDY_PRICE)%GUM_PRICE;
+    System.out.println("Number of Candies: "+candyAV);
+    System.out.println("Number of Gumballs: "+gumAV);
+        }else {
+             System.out.println("Not enough coupons");
+        }
+
+
+
+    }
 
 
 }
+
